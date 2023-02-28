ae0043649e4d975c76956d54685ca544e7c490ac
Erzhan Turdumambetov <erjanturdumambetov@gmail.com>
2023-02-28 19:46:45 +0600

    new-config-8

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_o0JFYAcul', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5np5oj8ni5skjia5ei5ignf7gh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_nnBAXgyVG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '14dgluhajtsgmjq8pjpa3hdhfo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
